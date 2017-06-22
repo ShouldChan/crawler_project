46b5a7c93331d6aa2df6af0007cb9e498ce7202c
ShouldChan <ezanmydream@163.com>
2017-06-22 22:24:34 +0800

    douban_8

--- Douban_downloadpic.py
+++ Douban_downloadpic.py
@@ -4,6 +4,7 @@ import requests
 import time
 import sys
 import urllib2
+import urllib
 import re
 from bs4 import BeautifulSoup
 
@@ -11,6 +12,9 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 pic_dir = './posters/'
+
+base_img_url = 'https://img3.doubanio.com/view/photo/raw/public/p'
+base_pid_suffix = 'photos?type=R'
 
 t = time.time()
 
@@ -27,54 +31,69 @@ def readHomepage(hg_list):
     print hg_list
 
 
-# 得到页面全部内容
-def askURL(url):
-    request = urllib2.Request(url)  # 发送请求
-    try:
-        response = urllib2.urlopen(request)  # 取得响应
-        html = response.read()  # 获取网页内容
-        # print html
-    except urllib2.URLError, e:
-        if hasattr(e, "code"):
-            print e.code
-        if hasattr(e, "reason"):
-            print e.reason
-    return html
+# 获取poster的ID 这个ID是用来加在高清海报图片后面下载链接的
+def getPID(hg_list, pid_list):
+    find_pid = re.compile(r'<li data-id="(.*?)">')  # get posterID
+    for hg_url in hg_list:
+        url = hg_url + base_pid_suffix
+        r = requests.get(url)
+        content = r.content.decode('utf8')
+        soup = BeautifulSoup(content, 'lxml')
+        for item in soup.find_all('div', class_='article'):
+            item = str(item)
+            pid = re.findall(find_pid, item)[0]
+            pid_list.append(pid)
+            # print pid
+    print pid_list
 
 
-def getPosterId(base_url):
-    find_pId = re.compile(r'<li data-id="(.*?)">')  # get posterID
-    # print find_pId
-    for i in range(0, 10):
-        html = askURL(base_url)
-        soup = BeautifulSoup(html, 'lxml')
-        for item in soup.findall('div', class_='item'):
-            data = []
+# def savePosterDownloadUrl_txt()
 
 
-def downloadPic(pic_list):
-    x = 0
-    fwrite_timeout = open(pic_dir + 'timeout_list.txt', 'ab')
-    for pic_url in pic_list:
-        x += 1
-        print x
-        filePath_txt = pic_dir + '%s.txt' % x
-        print pic_url
-        try:
-            rq = requests.get(pic_url, timeout=120)
-        except requests.exceptions.ConnectionError:
-            print 'md!download time out!Mark this and try again...'
-            fwrite_timeout.write(str(x) + '\t' + str(pic_url) + '\n')
-            print '----------------------------------------------------'
-            continue
-        filePath_pic = pic_dir + '%s.jpg' % x
+def downloadPic():
+    # piccontent = urllib2.urlopen('https://img3.doubanio.com/view/photo/raw/public/p480747492.jpg').read()
+    urllib.urlretrieve('https://movie.douban.com/photos/photo/480747492/', './1.jpg')
+    # fwrite_pic = open(pic_dir + '1.jpg', 'wb')
+    # fwrite_pic.write(piccontent)
+    # fwrite_pic.close()
 
-        fwrite_pic = open(filePath_pic, 'wb')
-        fwrite_pic.write(rq.content)
-        print time.time() - t
-        print '==========================='
+    # fwrite_timeout = open(pic_dir + 'timeout_list.txt', 'ab')
+    # try:
+    #     rq = requests.get('https://img3.doubanio.com/view/photo/raw/public/p480747492.jpg', timeout=120)
+    # except requests.exceptions.ConnectionError:
+    #     print 'md!download time out!Mark this and try again...'
+    #     fwrite_timeout.write(str(1) + '\n')
+    #     print '----------------------------------------------------'
+    # finally:
+    #     filePath_pic = pic_dir + '1.jpg'
+    #
+    #     fwrite_pic = open(filePath_pic, 'wb')
+    #     fwrite_pic.write(rq.content)
+    #     fwrite_pic.close()
+    # fwrite_timeout.close()
+    # x = 0
+    # fwrite_timeout = open(pic_dir + 'timeout_list.txt', 'ab')
+    # for pic_url in pic_list:
+    #     x += 1
+    #     print x
+    #     filePath_txt = pic_dir + '%s.txt' % x
+    #     print pic_url
+    #     try:
+    #         rq = requests.get(pic_url, timeout=120)
+    #     except requests.exceptions.ConnectionError:
+    #         print 'md!download time out!Mark this and try again...'
+    #         fwrite_timeout.write(str(x) + '\t' + str(pic_url) + '\n')
+    #         print '----------------------------------------------------'
+    #         continue
+    #     filePath_pic = pic_dir + '%s.jpg' % x
+    #
+    #     fwrite_pic = open(filePath_pic, 'wb')
+    #     fwrite_pic.write(rq.content)
+    #     print time.time() - t
+    #     print '==========================='
 
 
+# 读取下载数据集里面的第二列poster缩略图的下载链接（已经不用缩略图了 不够清晰）
 def readPicUrl(pic_list):
     with open('./douban_Top250.txt', 'rb') as fread:
         lines = fread.readlines()
@@ -85,19 +104,26 @@ def readPicUrl(pic_list):
             print pic_url
             pic_list.append(pic_url)
 
+# 模拟登陆豆瓣
+# def modelLogin():
+
 
 def main():
-    base_img = 'https://img3.doubanio.com/view/photo/raw/public/p'
-    pic_list = []
-    hg_list = []
-    test_hg = 'https://movie.douban.com/subject/1292262/'
-    getPosterId(test_hg)
-    # readHomepage(hg_list)
-    print '-------readHomepageUrl over-------'
-    # readPicUrl(pic_list)
-    print '-------readPicUrl over-------'
-    # downloadPic(pic_list)
-    print '-------downloadPic over------'
+    t = time.time()
+    pic_list = []  # 图片下载链接
+    hg_list = []  # 每部电影主页的链接
+    pid_list = []  # 获取poster的ID 加在base_img_url后面用于下载图片
+
+    readHomepage(hg_list)
+    print '-------readHomepageUrl over-------\t' + str(time.time() - t)
+
+    getPID(hg_list,pid_list)
+    print '-------getPID over-------\t' + str(time.time() - t)
+
+    downloadPic()
+    print '-------downloadPic over------\t' + str(time.time() - t)
+
+
 
 
 main()
