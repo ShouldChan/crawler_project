92e38fb693e094245f5afd9f5f7b07f04c31df95
ShouldChan <ezanmydream@163.com>
2017-07-31 14:52:48 +0800

    gui_2

--- gui_crawler.py
+++ gui_crawler.py
@@ -4,8 +4,13 @@ from ScrolledText import ScrolledText
 
 root = Tk()
 root.title('ShouldChan_crawler')
-text = ScrolledText(root, font=('微软雅黑', 10))# 文本滚动条
+root.geometry('+600+300')# 坐标，大小
+text = ScrolledText(root, font=('微软雅黑', 10))  # 文本滚动条
 text.grid()
 button = Button(root, text='Start', font=('微软雅黑', 10))
 button.grid()
+varl = StringVar()
+label = Label(root, font=('微软雅黑', 10), fg='blue',textvariable=varl)
+label.grid()
+varl.set('Ready...')
 root.mainloop()
