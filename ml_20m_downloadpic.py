72194904a21a85b9dfc8b952aacbf142c6f1f237
ShouldChan <ezanmydream@163.com>
2017-07-31 20:45:12 +0800

    ml_downloadpic_1

---
+++ ml_20m_downloadpic.py
@@ -0,0 +1,23 @@
+# coding:utf-8
+pic_dir = './ml_20m_posters/'
+mv_dir = './ml_20m/'
+
+import requests
+import time
+import pandas as pd
+
+
+# read imdbId to download the pics
+def read_imdbId():
+    header = ['movieId', 'imdbId', 'tmdbId']
+    df = pd.read_csv(mv_dir + 'links.csv', names=header)
+    imdbId_list = df['imdbId']
+    print imdbId_list
+    print type(imdbId_list)
+    imdb = df.imdbId.unique()
+    print imdb
+    n_imdbId = imdb.shape[0]
+    print n_imdbId
+
+
+read_imdbId()
