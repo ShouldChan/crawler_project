9f5569c6ead5c8f73f4dabac3d26c484f096d56e
ShouldChan <ezanmydream@163.com>
2017-07-31 20:49:55 +0800

    update_dpic_2

--- ml_20m_downloadpic.py
+++ ml_20m_downloadpic.py
@@ -11,14 +11,19 @@ import pandas as pd
 def read_imdbId():
     header = ['movieId', 'imdbId', 'tmdbId']
     df = pd.read_csv(mv_dir + 'links.csv', names=header)
-    imdbId_list = df['imdbId']
-    print imdbId_list
-    print type(imdbId_list)
+    imdbId_series = df['imdbId']
+    # print imdbId_series
+    print type(imdbId_series)
     # print imdbId_list[0]
     imdb = df.imdbId.unique()
-    print imdb
+    # print imdb
+    # n_imdbId-1 为imdbId的个数，即电影的个数
     n_imdbId = imdb.shape[0]
-    print n_imdbId
+    # print n_imdbId
+
+    return imdbId_series, n_imdbId
 
 
-read_imdbId()
+if __name__ == '__main__':
+    imdbid_series, n_imdbid = read_imdbId()
+    print imdbid_series,n_imdbid
