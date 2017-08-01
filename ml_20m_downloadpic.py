e8db15b4d0c963d08019b55e96ea45b5ac4031b6
ShouldChan <ezanmydream@163.com>
2017-08-01 09:20:27 +0800

    update_mlpic_4

--- ml_20m_downloadpic.py
+++ ml_20m_downloadpic.py
@@ -7,6 +7,11 @@ basis_back = '/mediaindex?refine=poster&ref_=ttmi_ref_pos'
 import requests
 import time
 import pandas as pd
+import re
+import urllib2
+from bs4 import BeautifulSoup
+import urllib
+import os
 
 
 # read imdbId to download the pics
@@ -32,13 +37,49 @@ def read_imdbId():
         print 'the number of movie id:\t', str(n_imdbId)
     # print n_imdbId
 
-    return imdbId_series, n_imdbId
+    return imdbId_series, movieId_series, n_imdbId
 
 
-def extract_posters(imdbid_series, n_imdbid):
+# 得到页面全部内容
+def askURL(url):
+    request = urllib2.Request(url)  # 发送请求
+    try:
+        response = urllib2.urlopen(request)  # 取得响应
+        html = response.read()  # 获取网页内容
+        # print html
+    except urllib2.URLError, e:
+        if hasattr(e, "code"):
+            print e.code
+        if hasattr(e, "reason"):
+            print e.reason
+    return html
+
+
+# 1、os.path.exists(path) 判断一个目录是否存在
+# 2、os.makedirs(path) 多层创建目录
+# 3、os.mkdir(path) 创建目录
+def extract_posters(imdbid_series, movieid_series, n_imdbid):
+    # findLink = re.compile(r'<a href="(.*?)">')  # 找到影片详情链接
+    findImgSrc = re.compile(r'<img.*src="(.*jpg)"', re.S)  # 找到影片图片
+    # findTitle = re.compile(r'<span class="title">(.*)</span>')  # 找到片名
+    # 去掉无关内容
+    # remove = re.compile(r'                            |\n|</br>|\.*')
+    for i in range(1, n_imdbid + 1):
+        print imdbid_series[i], movieid_series[i]
+        url = basis_font + str(imdbid_series[i]) + basis_back
+        html = askURL(url)
+        soup = BeautifulSoup(html, 'lxml')
+        for item in soup.find_all('div', class_='media_index_thumb_list'):  # 找到每一个影片项
+            item = str(item)
+            print item
+            imgSrc = re.findall(findImgSrc, item)[0]
+            print imgSrc
+            save_pic = pic_dir + str(imdbid_series[i]) + '.jpg'
+            urllib.urlretrieve(imgSrc, save_pic)
     print 'asdasd'
 
 
 if __name__ == '__main__':
-    imdbid_series, n_imdbid = read_imdbId()
-    print imdbid_series, n_imdbid
+    imdbid_series, movieid_series, n_imdbid = read_imdbId()
+    # print imdbid_series, n_imdbid
+    extract_posters(imdbid_series, movieid_series, n_imdbid)
