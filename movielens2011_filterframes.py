b2faa521f115200ba626234a8a1cdb37e5372b79
ShouldChan <ezanmydream@163.com>
2017-08-04 21:17:41 +0800

    filter_frames_3

--- movielens2011_filterframes.py
+++ movielens2011_filterframes.py
@@ -15,7 +15,7 @@ def read_imdbId():
         for line in lines:
             tempdata = line.strip().split('\t')
             movieid, imdbid = tempdata[0], tempdata[2]
-            print movieid, imdbid
+            # print movieid, imdbid
             dic[str(movieid)] = str(imdbid)
     # print dic['1']
     return dic
@@ -32,11 +32,28 @@ def read_movieId_need():
             id = temp[0]
             ID_list.append(id)
     # print ID_list
-    print len(ID_list)
+    print 'lengths of the movies need to use:', len(ID_list)
     return ID_list
 
 
 def traverse_folder(dic, Id_list):
+    fwrite = open(mv_dir + 'valid_movieid_imdbid.txt', 'wb')
     for i in Id_list:
-        path = pic_dir + str(dic[str(i)]) + '.jpg'
-    print 'sdfasdfsda'
+        path = pic_dir + str(dic[str(i)]) + '/'
+        if os.path.exists(path) == False:
+            print 'not exist this folder.........'
+        else:
+            for root, dirs, files in os.walk(path):  # 获取目录下文件的个数 如果该文件夹为空就删除并记录写入一个描述文件
+                if len(files) == 0:
+                    print '--------------this folder is empty--------------'
+                else:
+                    fwrite.write(str(i) + '\t' + str(dic[str(i)]) + '\n')
+                    print files
+        print i, dic[str(i)]
+    print 'traverse over...'
+
+
+if __name__ == '__main__':
+    dic = read_imdbId()
+    Id_list = read_movieId_need()
+    traverse_folder(dic, Id_list)
